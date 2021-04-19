0459362b59dffcbce19417f33d34fcea6c7bb5e2
RobertRoccio <robertroccio11@gmail.com>
2021-04-19 07:49:01 -0700

    Video Get Method

--- webapp/app.py
+++ webapp/app.py
@@ -4,6 +4,10 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
-    return "Flask Server & Android are Working Successfully"
+    videofile = flask.request.files['video']
+    filename = werkzeug.utils.secure_filename(videofile.filename)
+    print("\nReceived video: " + videofile.filename)
+    videofile.save(filename)
+    return "Video Uploaded Successfully"
 
 app.run(host="0.0.0.0", port=5000, debug=True)
